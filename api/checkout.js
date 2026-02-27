a885a27b72a4cdabef1f38922fcdbe652297fe08
marcel-ceceu <marcel.franciscato94@gmail.com>
2026-02-27 12:54:45 -0300

    Fix formatting and add newline at end of file

--- api/checkout.js
+++ api/checkout.js
@@ -1,4 +1,4 @@
-﻿export default async function handler(req, res) {
+export default async function handler(req, res) {
   if (req.method === 'OPTIONS') {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
